ecd3930200280e3ee2c4635203e0c9f7d4636902
Martin Jackson <mjackson@equalexperts.com>
2013-02-28 10:16:26 +0000

    Refactoring sensuchecks module to fit librarian module which does not appear to support recursive git submodules, this is probably a good thing

--- manifests/common.pp
+++ manifests/common.pp
@@ -5,12 +5,12 @@ class sensuchecks::common {
     mode  => '0755'
   }
   file { '/etc/sensu/plugins/check-procs.rb':
-    source => 'puppet:///modules/sensuchecks/sensu-community-plugins/plugins/processes/check-procs.rb',
+    source => 'puppet:///sensu-plugins/plugins/processes/check-procs.rb',
   }
   file { '/etc/sensu/plugins/check-http.rb':
-    source => 'puppet:///modules/sensuchecks/sensu-community-plugins/plugins/http/check-http.rb',
+    source => 'puppet:///sensu-plugins/plugins/http/check-http.rb',
   }
   file { '/etc/sensu/plugins/check-disk.rb':
-    source => 'puppet:///modules/sensuchecks/sensu-community-plugins/plugins/system/check-disk.rb',
+    source => 'puppet:///sensu-plugins/plugins/system/check-disk.rb',
   }
 }
